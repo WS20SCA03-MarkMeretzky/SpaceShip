32de286e51d7bd4b4050ba0bf16c818d242e1a28
Mark Meretzky <mark.meretzky@gmail.com>
2020-05-02 12:12:04 -0400

    Initial Commit

---
+++ SpaceShip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceShip
+//
+//  Created by Mark Meretzky on 5/2/20.
+//  Copyright © 2020 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
